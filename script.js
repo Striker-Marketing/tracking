b0c16f0d31a2f64e21165bb20c11307530696861
deigo-lps <diego.lopes0909@gmail.com>
2026-07-22 15:08:29 -0300

    remove umami

--- script.js
+++ script.js
@@ -61,9 +61,9 @@ const initStrikerTracking = () => {
     if (userId) return userId;
     userId = uuidv4();
     setCookie("striker_user_id", userId);
-    if (window.umami) {
-      window.umami.identify(userId);
-    }
+    // if (window.umami) {
+    //   window.umami.identify(userId);
+    // }
     return userId;
   };
 
