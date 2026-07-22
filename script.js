e3a27dd119090169039d849804003542323167c8
deigo-lps <diego.lopes0909@gmail.com>
2026-07-22 13:23:53 -0300

    previne disparar em form de facebook

--- script.js
+++ script.js
@@ -87,6 +87,8 @@ const initStrikerTracking = () => {
 
   document.addEventListener("submit", async (e) => {
     const form = e.target;
+    const action = form.getAttribute("action");
+    if (action && action.includes("facebook")) return;
     const dataLayerObj = {};
     const userIp = getCookie("user_ip");
     if (userIp) dataLayerObj["user_ip"] = userIp;
